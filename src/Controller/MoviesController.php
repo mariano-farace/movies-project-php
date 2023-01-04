653f7614d9567f85c14ab4d6ca8f945746552cf2
Mariano <mariano_farace@hotmail.com>
2023-01-04 17:56:46 +0100

    Install webpack

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
+
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
     #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
-        $movies = ["Avengers", "Inception", "Loki", "Black Widow"];
 
-        return $this->render('index.html.twig', array("movies" => $movies));
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findBy([], ["id" => "DESC"]);
+
+        dd($movies);
+
+        return $this->render('index.html.twig');
     }
 }
